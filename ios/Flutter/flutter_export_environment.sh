1f35066b89a44b0ba26431174d697741f28718f2
smartshk <smartshk98@gmail.com>
2019-09-21 21:28:40 +0900

    add all the source code for my flutter app

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\Owner\Downloads\flutter_windows_v1.9.1+hotfix.2-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Owner\AndroidStudioProjects\material-components-flutter-codelabs-104-starter_and_103-complete\mdc_100_series"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\Owner\Downloads\flutter_windows_v1.9.1+hotfix.2-stable\flutter\bin\cache\artifacts\engine\ios"
